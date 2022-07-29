1f757656c25d387a603d54100fd38e9113b0bb1f
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-07-29 14:05:50 -0500

    roary: new package

--- packages/roary/package.py
+++ packages/roary/package.py
@@ -1,44 +1,57 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install roary
-#
-# You can edit this file again by typing:
-#
-#     spack edit roary
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
-from spack import *
+from spack.package import *
 
 
 class Roary(Package):
-    """FIXME: Put a proper description of your package here."""
-
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    """Rapid large-scale prokaryote pan genome analysis"""
+    
+    homepage = "https://sanger-pathogens.github.io/Roary/"
     url      = "https://github.com/sanger-pathogens/Roary/archive/refs/tags/v3.13.0.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('3.13.0', sha256='375f83c8750b0f4dea5b676471e73e94f3710bc3a327ec88b59f25eae1c3a1e8')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    depends_on('perl@5.8:5.9,5.11.5.13:',   type=('build', 'run'))
+    depends_on('perl-array-utils',   type=('build', 'run'))
+    depends_on('perl-bioperl',   type=('build', 'run'))
+    depends_on('perl-exception-class',   type=('build', 'run'))
+    depends_on('perl-graph',   type=('build', 'run'))
+    depends_on('perl-file-slurper',   type=('build', 'run'))
+    depends_on('perl-file-which',   type=('build', 'run'))
+    depends_on('perl-log-log4perl',   type=('build', 'run'))
+    depends_on('perl-moose',   type=('build', 'run'))
+    depends_on('perl-text-csv',   type=('build', 'run'))
+    depends_on('perl-perlio-utf8-strict',   type=('build', 'run'))
+    depends_on('perl-devel-overloadinfo',   type=('build', 'run'))
+    
+    #TODO add the following perl dependencies:
+    # File::Basename
+    # File::Copy
+    # File::Find::Rule
+    # File::Grep
+    # File::Path
+    # File::Spec
+    # File::Temp
+    # FindBin
+    # Getopt::Long
+    # Graph::Writer::Dot
+    # List::Util
+    # Moose::Role
+    # Digest::MD5::File
 
+    depends_on('bedtools2',   type=('build', 'run'))
+    depends_on('cdhit',   type=('build', 'run'))
+    depends_on('blast-plus',   type=('build', 'run'))
+    depends_on('mcl',   type=('build', 'run'))
+    depends_on('parallel',   type=('build', 'run'))
+    depends_on('prank',   type=('build', 'run'))
+    depends_on('mafft',   type=('build', 'run'))
+    depends_on('fasttree',   type=('build', 'run'))
+    
     def install(self, spec, prefix):
         # FIXME: Unknown build system
         make()
