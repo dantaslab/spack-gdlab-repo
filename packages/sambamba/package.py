714ac4f960faf963e16fd6c03ac555e8db953a2a
Luke Diorio-Toth <ldioriototh@gmail.com>
2022-09-19 23:30:56 -0500

    updated multiqc with working/new version

--- packages/sambamba/package.py
+++ packages/sambamba/package.py
@@ -10,10 +10,13 @@ class Sambamba(Package):
     """Sambamba: process your BAM data faster (bioinformatics)"""
 
     homepage = "https://lomereiter.github.io/sambamba/"
-    git = "https://github.com/lomereiter/sambamba.git"
+    url      = "https://github.com/biod/sambamba/archive/refs/tags/v0.8.2.tar.gz"
 
-    version("0.8.2", tag="v0.8.2", submodules=True)
-    version("0.6.6", tag="v0.6.6", submodules=True)
+    version('0.8.2', sha256='b4934f2bfdf07b12856e9c7a0634db0a4f6f4b05e2d4d9f0784bab26159590c1')
+    #git = "https://github.com/lomereiter/sambamba.git"
+
+    #version("0.8.2", tag="v0.8.2", submodules=True)
+    #version("0.6.6", tag="v0.6.6", submodules=True)
 
     depends_on("ldc~shared", type=("build", "link"))
     depends_on("python", type="build")
