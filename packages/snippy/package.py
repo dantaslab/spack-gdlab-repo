eaa73801be39866a3d1cd79bbe1a5471f1790144
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-07-07 15:53:10 -0500

    Minor change in samtools version used - downgraded from 1.17 to 1.14

--- packages/snippy/package.py
+++ packages/snippy/package.py
@@ -1,9 +1,10 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.package import *
+
+from spack import *
 
 
 class Snippy(Package):
@@ -12,13 +13,11 @@ class Snippy(Package):
     homepage = "https://github.com/tseemann/snippy"
     url = "https://github.com/tseemann/snippy/archive/refs/tags/v4.6.0.tar.gz"
 
-    license("GPL-2.0")
-
-    version("4.6.0", sha256="7264e3819e249387effd3eba170ff49404b1cf7347dfa25944866f5aeb6b11c3")
+    version('4.6.0', sha256='7264e3819e249387effd3eba170ff49404b1cf7347dfa25944866f5aeb6b11c3')
 
     depends_on("perl@5.26:", type="run")
     depends_on("perl-bioperl", type="run")
-    depends_on("samtools@1.17", type="run")
+    depends_on("samtools@1.14", type="run")
     
     def install(self, spec, prefix):
         mkdirp(prefix.bin)
