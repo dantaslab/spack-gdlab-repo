a98655d5deaf743021a920dc5d5b3f01a020562c
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-07-07 16:06:46 -0500

    Removed license metadata; note that license metadata is not present until version 0.21.0

--- packages/snippy/package.py
+++ packages/snippy/package.py
@@ -12,7 +12,6 @@ class Snippy(Package):
 
     homepage = "https://github.com/tseemann/snippy"
     url = "https://github.com/tseemann/snippy/archive/refs/tags/v4.6.0.tar.gz"
-    license("GPL-2.0")
     version('4.6.0', sha256='7264e3819e249387effd3eba170ff49404b1cf7347dfa25944866f5aeb6b11c3')
 
     depends_on("perl@5.26:", type="run")
