f9d17d1c17644b48f608221ab9478e2087a6eee3
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2024-08-29 14:53:42 -0500

    Created spack package for modified deconseq to utilize multiple CPUs

---
+++ packages/deconseq-multi/package.py
@@ -0,0 +1,39 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class DeconseqMulti(Package):
+    """The DeconSeq tool can be used to automatically detect and efficiently
+    remove sequence contaminations from genomic and metagenomic datasets; optimized for multithreaded usage."""
+
+    homepage = "https://github.com/dantaslab/deconseq-multi"
+    url      = "https://github.com/dantaslab/deconseq-multi/releases/download/v1.0.0/deconseq-multi-v1.0.0.tar.gz"
+
+    version('1.0.0', sha256='d40ea902d9f0ea86e7e0890d948f1e9516d767f98011d588cd6828c3b125309d')
+
+    depends_on("perl@5:")
+
+    def install(self, spec, prefix):
+        filter_file(r"#!/usr/bin/perl", "#!/usr/bin/env perl", "deconseq.pl")
+        filter_file(r"#!/usr/bin/perl", "#!/usr/bin/env perl", "splitFasta.pl")
+
+        mkdirp(prefix.bin)
+        install("bwa64", prefix.bin)
+        install("bwaMAC", prefix.bin)
+        install("deconseq.pl", prefix.bin)
+        install("splitFasta.pl", prefix.bin)
+        install("DeconSeqConfig.pm", prefix)
+
+        chmod = which("chmod")
+        chmod("+x", join_path(prefix.bin, "bwa64"))
+        chmod("+x", join_path(prefix.bin, "bwaMAC"))
+        chmod("+x", join_path(prefix.bin, "deconseq.pl"))
+        chmod("+x", join_path(prefix.bin, "splitFasta.pl"))
+
+    def setup_run_environment(self, env):
+        env.prepend_path("PERL5LIB", self.prefix)
